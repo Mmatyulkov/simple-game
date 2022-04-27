32422ff396295ced2a7d419a1af269335c107b43
Mmatyulkov <mmatyulkov@icloud.com>
2022-04-27 15:01:16 +0400

    Do normal way for files

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function play(rValue, pValue) {
-    document.querySelector('.left img').src="/" + pValue + "-hand.svg"
+function play(pValue) {
+    document.querySelector('.left img').src="./" + pValue + "-hand.svg"
+    scoreNumbers(result)
+    const result = valueCompare(pValue, rValue);
+    const rValue = enemyRandom()
     searchRock()
-    valueCompare(pValue, rValue)
-    scoreNumbers()
 }
 
 const wins = {
@@ -10,42 +11,24 @@ const wins = {
     'scissors': 'paper',
     'paper': 'rock',
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+function valueCompare (pValue, rValue) {
 
-function valueCompare (igor, valera) {
-
-    if (igor === valera){
+    if (pValue === rValue){
         console.log('Draw')
         return 0
     } 
-    
-    const bWins = wins[valera]
-    
-
-    if (bWins === igor){
+    const bWins = wins[rValue]
+    if (bWins === pValue){
         console.log('Lose')
         computerScore += 1
         return -1
     } 
-    
         humanScore += 1
         return 1
 } 
 
-function searchRock() {
-    let rocks = document.querySelectorAll('.choice-icons')
-    
-    
-    for (let i = 0; i < rocks.length; i++) {
-        let rock = rocks[i]
-        if (rock.getAttribute('src') === '/rock-hand.svg' || rock.getAttribute('src') === '/rock-hand-reverse.svg') {
-           rock.classList.add('rockIcon')
-        } else {
-            rock.classList.remove('rockIcon')
-        }
-    }
-}
-
-function scoreNumbers() {
+function scoreNumbers(result) {
     let humanPic = document.querySelector('.leftNumberIcon')
     humanPic.src = pictures[humanScore]
 
@@ -58,29 +41,35 @@ function scoreNumbers() {
     }
 }
 
-const pictures = {0:'/number0.svg', 1:'/number1.svg', 2:'/number2.svg', 3:'/number3.svg', 4:'/number4.svg', 5:'/number5.svg',}
+const pictures = {0:'./number0.svg', 1:'./number1.svg', 2:'./number2.svg', 3:'./number3.svg', 4:'./number4.svg', 5:'./number5.svg',}
+let humanScore = 0;
+let computerScore = 0; 
 
 function getPictureByScore(score){  
     return pictures[score];
-}
+}  
 
-let humanScore = 0;
-let computerScore = 0;   
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function enemyRandom() {
     let array = ['rock', 'scissors', 'paper']
     let rand = Math.floor(Math.random()*array.length)
     let rValue = array[rand]
     console.log(rValue)
-    
-    if (rValue === 'rock') {
-        document.querySelector('.right img').src="/rock-hand-reverse.svg"
-    } if (rValue === 'scissors') {
-        document.querySelector('.right img').src="/scissors-hand-reverse.svg"
-    } if (rValue === 'paper') {
-        document.querySelector('.right img').src="/paper-hand-reverse.svg"
-    }
-    
+    document.querySelector('.right img').src="./" + rValue + "-hand-reverse.svg"
     return rValue;
 } 
+
+function searchRock() {
+    let rocks = document.querySelectorAll('.choice-icons')
+    
+    
+    for (let i = 0; i < rocks.length; i++) {
+        let rock = rocks[i]
+        if (rock.getAttribute('src') === './rock-hand.svg' || rock.getAttribute('src') === './rock-hand-reverse.svg') {
+           rock.classList.add('rockIcon')
+        } else {
+            rock.classList.remove('rockIcon')
+        }
+    }
+}
